8101c430ff8c2344144dfa5772aea92fabcb7cf6
Tim Caswell <tim@creationix.com>
2011-03-15 00:58:23 -0700

    Start to implement protocol logic

--- encoder.js
+++ encoder.js
@@ -1,37 +1,16 @@
+eval(require('./constants'));
+var Formats = require('./formats');
+var Util = require('util');
 
-function writeInt32(buffer, offset, value) {
-  buffer[offset] = value >>> 24;
-  buffer[offset + 1] = value >>> 16;
-  buffer[offset + 2] = value >>> 8;
-  buffer[offset + 3] = value;
-}
-function writeInt64(buffer, offset, value) {
-  var high = Math.floor(value / 0x100000000);
-  var low = value - high * 0x100000000;
-  writeInt32(buffer, offset, high);
-  writeInt32(buffer, offset + 4, low);
-}
-function writeString(buffer, offset, string) {
-  var length = string.length;
-  writeInt32(buffer, offset, length);
-  buffer.write(string, "ascii", offset + 4, string);
-}
-function writeAttrs(buffer, offset, attrs) {
-  writeInt32(buffer, offset, 15); // ATTR_SIZE | ATTR_UIDGID | ATTR_PERMISSIONS | ACMODTIME);
-  writeInt64(buffer, offset + 4, attrs.size); // present only if flag ATTR_SIZE
-  writeInt32(buffer, offset + 12, attrs.uid); // present only if flag ATTR_UIDGID
-  writeInt32(buffer, offset + 16, attrs.gid); // present only if flag ATTR_UIDGID
-  writeInt32(buffer, offset + 20, attrs.permissions); // present only if flag ATTR_PERMISSIONS
-  writeInt32(buffer, offset + 24, attrs.atime); // present only if flag ACMODTIME
-  writeInt32(buffer, offset + 28, attrs.mtime); // present only if flag ACMODTIME
-}  
+module.exports = encode;
 
-
-function encode(typeName, args) {
+function encode(type, args) {
+  var typeName = FXP_LOOKUP[type];
   var format = Formats[typeName];
   if (!format) { throw new Error("Unknown format " + typeName); }
-  var type = Codes[typeName];
-  if (args.length !== format.length) { throw new Error("Expected %s args for %s, but only got %s args", format.length, typeName, args.length); }
+  if (args.length !== format.length) { throw new Error(
+    "Expected " + format.length + " args for " + typeName + ", but only got " + args.length + " args"
+  ); }
   
   // Calculate body length
   var length = 1;
@@ -45,6 +24,13 @@ function encode(typeName, args) {
         break;
       case "ATTRS":
         length += 32;
+        break;
+      case "PAIRS":
+        var keys = Object.keys(args[i]);
+        for (var j = 0, ll = keys.length; j < ll; j++) {
+          var key = keys[j];
+          length += 8 + key.length + value[key].length;
+        }
         break;
       default: throw new Error("Unknown type " + Util.inspect(format[i]));
     }
@@ -61,22 +47,67 @@ function encode(typeName, args) {
     var value = args[i];
     switch (format[i]) {
       case "uint32":
-        writeInt32(buffer, offset, value);
-        offset += 4;
+        offset += writeInt32(buffer, offset, value);
+        break;
+      case "uint64":
+        offset += writeInt64(buffer, offset, value);
         break;
       case "string":
-        writeString(buffer, offset, value);
-        offset += value.length + 4;
+        offset += writeString(buffer, offset, value);
         break;
       case "ATTRS":
-        writeAttrs(buffer, offset, value);
-        offset += 32;
+        offset += writeAttrs(buffer, offset, value);
+        break;
+      case "PAIRS":
+        offset += writePairs(buffer, offset, value);
         break;
       default: throw new Error("Unknown type " + Util.inspect(format[i]));
     }
   }
   
   return buffer;
+}
+
+
+function writeInt32(buffer, offset, value) {
+  buffer[offset] = value >>> 24;
+  buffer[offset + 1] = value >>> 16;
+  buffer[offset + 2] = value >>> 8;
+  buffer[offset + 3] = value;
+  return 4;
+}
+function writeInt64(buffer, offset, value) {
+  var high = Math.floor(value / 0x100000000);
+  var low = value - high * 0x100000000;
+  writeInt32(buffer, offset, high);
+  writeInt32(buffer, offset + 4, low);
+  return 8;
+}
+function writeString(buffer, offset, string) {
+  var length = string.length;
+  writeInt32(buffer, offset, length);
+  buffer.write(string, "ascii", offset + 4, string);
+  return length + 4;
+}
+function writeAttrs(buffer, offset, attrs) {
+  writeInt32(buffer, offset, 15); // ATTR_SIZE | ATTR_UIDGID | ATTR_PERMISSIONS | ACMODTIME);
+  writeInt64(buffer, offset + 4, attrs.size); // present only if flag ATTR_SIZE
+  writeInt32(buffer, offset + 12, attrs.uid); // present only if flag ATTR_UIDGID
+  writeInt32(buffer, offset + 16, attrs.gid); // present only if flag ATTR_UIDGID
+  writeInt32(buffer, offset + 20, attrs.permissions); // present only if flag ATTR_PERMISSIONS
+  writeInt32(buffer, offset + 24, attrs.atime); // present only if flag ACMODTIME
+  writeInt32(buffer, offset + 28, attrs.mtime); // present only if flag ACMODTIME
+  return 32;
+}  
+function writePairs(buffer, offset, pairs) {
+  var keys = Object.keys(pairs);
+  var start = offset;
+  for (var i = 0, l = keys.length; i < l; i++) {
+    var key = keys[i];
+    offset += writeString(buffer, offset, key);
+    offset += writeString(buffer, offset, pairs[key]);
+  }
+  return offset - start;
 }
 
 
--- sftp.js
+++ sftp.js
@@ -1,11 +1,12 @@
 var Net = require('net'),
     ChildProcess = require('child_process'),
     Util = require('util'),
-    createParser = require('./parser');
+    createParser = require('./parser'),
+    encode = require('./encoder');
 eval(require('./constants'));
 
 // Proxy to real sftp server and trace conversation
-function Proxy(client) {
+function proxy(client) {
   var child = ChildProcess.spawn("/usr/lib/openssh/sftp-server", ["-e"]);
   client.pipe(child.stdin);
   child.stdout.pipe(client);
@@ -24,7 +25,50 @@ function Proxy(client) {
   });
 }
 
-Net.createServer(Proxy).listen(6000);
+function real(client) {
+  var scope = {
+    send: function send(type) {
+      var args = Array.prototype.slice.call(arguments, 1);
+      var chunk = encode(type, args);
+      console.log("OUT " + FXP_LOOKUP[type] + " " + Util.inspect(args, false, 3));
+      console.log("OUT " + chunk.inspect());
+      client.write(chunk);
+    },
+    status: function status(id, code, message) {
+      scope.send(FXP_STATUS, id, code, message, "");
+    },
+    close: function () {
+      client.close();
+    }
+  };
+  client.on('data', function (chunk) {
+    console.log("IN  " + chunk.inspect());
+  });
+  createParser(client, function (type, args) {
+    console.log("IN  " + FXP_LOOKUP[type] + " " + Util.inspect(args, false, 3));
+    var typeName = FXP_LOOKUP[type];
+    if (Handlers.hasOwnProperty(typeName)) {
+      Handlers[typeName].apply(scope, args);
+    } else {
+      throw new Error("Unknown type " + typeName);
+    }
+    
+  });
+}
+
+var Handlers = {
+  INIT: function (version) {
+    if (version === 3) {
+      this.send(FXP_VERSION, 3, {});
+    } else {
+      throw new Error("Invalid client version " + version);
+    }
+  }
+};
+
+
+
+Net.createServer(real).listen(6000);
 console.log("sftp-server listening on port 6000");
 
 
